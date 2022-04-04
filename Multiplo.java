33d43954d919c2c6edefa77c1ea501259923c82d
brunotakeda <takedabruno@outlook.com>
2022-04-04 17:22:59 +0000

    Multiplo

---
+++ Multiplo.java
@@ -0,0 +1,11 @@
+public class Multiplo{
+    
+    public static void main (String[] args){
+        int valor=6;
+
+        for(int i = 0; i < 100; i+=6){
+            System.out.println( i );
+
+        }
+    }   
+}    
