c152a14380944eafdfdeb1df104381c3346e237d
brunotakeda <takedabruno@outlook.com>
2022-04-04 17:55:27 +0000

    parte3

---
+++ Brasil.java
@@ -0,0 +1,25 @@
+public class Brasil{
+
+    public static void main (String [] args){
+    String V[] = new String[5];
+
+    V[0] = "Soja";
+    V[1] = "Carne";
+    V[2] = "arroz";
+    V[3] = "Feijao";
+    V[4] = "Cafe";
+
+    int p[] = new int[5];
+    p[0] = 17800;
+    p[1] =43250;
+    p[2] =6000;
+    p[3] =8000;
+    p[4] =4000;
+
+    System.out.println(V[0] + " Tem o preço ="+p[0]);
+    System.out.println(V[1] + " Tem o preço ="+p[1]);
+    System.out.println(V[2] + " Tem o preço ="+p[2]);
+    System.out.println(V[3] + " Tem o preço ="+p[3]);
+    System.out.println(V[4] + " Tem o preço ="+p[4]);
+    }
+}
