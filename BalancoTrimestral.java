115c433a9aae42ade7dde25f6fe0fdae95e8705d
brunotakeda <takedabruno@outlook.com>
2022-04-04 17:05:30 +0000

    prova

---
+++ BalancoTrimestral.java
@@ -0,0 +1,9 @@
+public class BalancoTrimestral{
+    public static void main (String[] args){
+        int salario;
+        int gastosAbril= 1200;
+
+        System.out.println (gastosAbril*10);
+
+    }
+}
